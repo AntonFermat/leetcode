d43b9be8ea807b7fd9bd50f9dfdab3a5da64e610
antonfermat <apichugin@gmail.com>
2023-11-01 13:45:01 +0900

    2433. Find The Original Array of Prefix Xor

--- src/main/kotlin/leetcode/p2400/p2433/SolutionKt.kt
+++ src/main/kotlin/leetcode/p2400/p2433/SolutionKt.kt
@@ -1,10 +1,5 @@
 package leetcode.p2400.p2433
 
 class SolutionKt {
-    fun findArray(pref: IntArray): IntArray {
-        val res = IntArray(pref.size)
-        res[0] = pref[0]
-        for (i in 1 until pref.size) res[i] = pref[i - 1] xor pref[i]
-        return res
-    }
+    fun findArray(pref: IntArray): IntArray = IntArray(pref.size) { pref[it] xor pref.getOrElse(it - 1) { 0 } }
 }
