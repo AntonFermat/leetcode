1e64391660bf3b5db770fe826c1bd491706bdffc
antonfermat <apichugin@gmail.com>
2023-11-27 15:31:23 +0900

    935. Knight Dialer

--- src/main/kotlin/leetcode/p0900/p0935/SolutionKt.kt
+++ src/main/kotlin/leetcode/p0900/p0935/SolutionKt.kt
@@ -1,9 +1,8 @@
 package leetcode.p0900.p0935
 
 class SolutionKt {
-    fun knightDialer(n: Int): Int {
-        return if (n == 1) return 10 else ((0..n - 2).fold(listOf(4L, 2, 2, 1)) { l, _ ->
-            listOf(2 * l[1] + 2 * l[2], l[0], l[0] + 2 * l[3], l[2]).map { it % 1_000_000_007 }
-        }.sum() % 1_000_000_007).toInt()
-    }
+    fun knightDialer(n: Int): Int =
+        if (n == 1) 10 else ((0..n - 2).fold(listOf(4L, 2, 2, 1))
+            { l, _ -> listOf(2 * l[1] + 2 * l[2], l[0], l[0] + 2 * l[3], l[2]).map { it % 1_000_000_007 }}
+            .sum() % 1_000_000_007).toInt()
 }
